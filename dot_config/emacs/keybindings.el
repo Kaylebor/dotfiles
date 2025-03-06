214b2ec6f3a6885b63d5af2c66c7ea39fab4a258
Ender Veiga Bueno <letstalk@ender.codes>
2025-03-06 17:05:57 +0100

    Emacs - add Evil

--- dot_config/emacs/keybindings.el
+++ dot_config/emacs/keybindings.el
@@ -1,5 +1,23 @@
 ;; -*- lexical-binding: t; -*-
 
+(use-package evil :ensure t
+  :custom
+  (evil-want-keybinding nil)
+  (evil-want-integration t)
+  :init
+  (evil-mode 1)
+)
+
+(use-package evil-collection :ensure t
+  :init
+  (evil-collection-init)
+)
+
+(with-eval-after-load 'evil
+  ;; Replace M-: with pp-eval-expression
+  (keymap-set evil-normal-state-map "M-:" #'pp-eval-expression)
+)
+
 ;; Keybinds for completion-preview
 (with-eval-after-load 'completion-preview
   ;; Cycle the completion candidate that the preview shows
