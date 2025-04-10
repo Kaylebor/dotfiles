dc0180e146ef8b3eb34cc665822cf58c9bf652c8
Ender Veiga Bueno <letstalk@ender.codes>
2025-04-10 13:26:14 +0200

    Remove TERM variable from ec alias

--- dot_config/fish/functions/ec.fish
+++ dot_config/fish/functions/ec.fish
@@ -1,3 +1,3 @@
 function ec --wraps='TERM=xterm-256color emacsclient -t' --description 'alias ec TERM=xterm-256color emacsclient -t'
-  TERM=xterm-256color emacsclient -t $argv
+    emacsclient -t $argv
 end
